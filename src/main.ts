310daa6e52417fd100ed82b2b904138db70677fb
Taufan Arif <taufan.arif@moxiworks.com>
2022-09-05 21:17:16 +0700

    learn update 2 sep 2022 assignment

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { ServerModule } from './app/server/server.module';
+import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(ServerModule)
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
