fdbb6e5c07c7923f8081a01ce6949e8bfede1c79
Taufan Arif <taufan.arif@moxiworks.com>
2023-09-01 17:31:31 +0700

    change app name to jira-pothijo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-dream-app'`, () => {
+  it(`should have as title 'jira-pothijo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-dream-app');
+    expect(app.title).toEqual('jira-pothijo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-dream-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('jira-pothijo app is running!');
   });
 });
