f29c9757ff421b04443945a770d70bb24671ecb5
Taufan Arif <taufan.arif@moxiworks.com>
2023-09-01 19:23:40 +0700

    generate bootstrap and make components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { BoardComponent } from './pages/board/board.component';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { UsersComponent } from './pages/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ProjectsComponent,
+    BoardComponent,
+    LoginComponent,
+    LayoutComponent,
+    UsersComponent
+],
   imports: [
     BrowserModule,
     AppRoutingModule
